f016b79f962c11c187f1e1765bc3ea91b30f7eb9
mofan <chenyangyf@seeyon.com>
2022-03-23 21:28:27 +0800

    整理时补充

--- BasicGrammar/DataType.lua
+++ BasicGrammar/DataType.lua
@@ -47,7 +47,7 @@ end
 
 print("---------------- ")
 -- number
--- Lua 默认只有一种 number 类型
+-- Lua 默认只有一种 number 类型，即：double（双精度）类型
 print(type(2))
 print(type(2.2))
 print(type(0.2))
@@ -77,10 +77,10 @@ print(123 .. 456) -- 123456
 print("---------------- ")
 -- table
 --[[ 
-    1、table 的创建是通过"构造表达式"来完成，最简单构造表达式是{}，用来创建一个空表。也可以在表里添加一些数据，直接初始化表
-    2、table 其实是一个"关联数组"（associative arrays），数组的索引可以是数字或者是字符串。
-    3、table 的默认索引从 1 开始，而不是 0
-    4、table 不会固定长度大小，有新数据添加时 table 长度会自动增长，没初始的 table 都是 nil
+    1、table 的创建是通过"构造表达式"来完成，最简单构造表达式是{}，用来创建一个空表。也可以在表里添加一些数据，直接初始化表；
+    2、table 其实是一个"关联数组"（associative arrays），数组的索引可以是数字或者是字符串；
+    3、table 的默认索引从 1 开始，而不是 0；
+    4、table 不会固定长度大小，有新数据添加时 table 长度会自动增长，没初始的 table 都是 nil。
  ]]
 local table1 = {}
 local table2 = {"apple", "pear", "banana"}
@@ -130,6 +130,8 @@ testFun(tab, function (k, v) -- 匿名函数作为入参
     return k .. "= " .. v
 end)
 
+print("---------------- ")
+
 --[[ 
     全局变量：全局变量在代码运行周期从头到尾，都不会被销毁，而且随处都可调用。
     当代码量增加，很多时候大量新建全局变量会导致内存激增，因此需要一种可以临时使用、并且可以自动销毁释放内存资源的变量。
@@ -139,11 +141,11 @@ end)
 -- 全局变量与 table
 -- 在 Lua 中，所有的全局变量全部被存放在了一共大 table 中，这个 table 名为：_G
 A = 123
-print("使用 _G 输出全局变量 A 的值 ", _G.A)
+print("使用 _G 输出全局变量 A 的值 ", _G.A) -- 123
 _G.B = 234 -- 相当于定义了一个全局变量
-print("获取使用 _G 定义的全局变量 ", _G.B)
+print("获取使用 _G 定义的全局变量 ", _G.B) -- 234
 _G["def"] = 456
-print("获取全局变量 def 对应的值 ", def, _G["def"], _G.def)
+print("获取全局变量 def 对应的值 ", def, _G["def"], _G.def) -- 456 456 456
 -- 甚至还可以
-_G.print("hello") -- 相当于直接调用 print()
-_G["print"]("world") -- 相当于直接调用 print()
+_G.print("hello") -- 相当于直接调用 print("hello")
+_G["print"]("world") -- 相当于直接调用 print("world")
